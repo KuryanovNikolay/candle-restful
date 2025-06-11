af2625e0c66c89717de960c7063e8a19b8714cd2
Nikolaus <nikolaykuryanov2004@gmail.com>
2025-06-11 04:49:20 +0300

    test controller

---
+++ WebApplication1/Controllers/OrdersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+
+namespace WebApplication1.Controllers;
+
+[ApiController]
+[Route("orders")]
+public class OrdersController : ControllerBase
+{
+    private List<Order> _orders = new List<Order>
+    {
+        new Order(1, "Order 1"),
+        new Order(2, "Order 2")
+    };
+
+    [HttpGet]
+    public IActionResult GetOrderById([FromQuery]int id)
+    {
+        var order = _orders.FirstOrDefault(o => o.Id == id);
+        return Ok(order);
+    }
+
+}
---
+++ WebApplication1/Data/Order.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplication1.Data;
+
+public class Order
+{
+    public Order(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    public int Id { get; private set; }
+
+    public string Name { get; private set; }
+
+}
+
